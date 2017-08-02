8609c46c9e4569f22702bceaa64e7802b07177e5
Hanse <lhs2281@nate.com>
2017-08-02 11:02:23 +0900

    df
    df

---
+++ app/src/main/java/samsung/membership/splash/ImageAdapter.java
@@ -0,0 +1,13 @@
+package samsung.membership.splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_adapter);
+    }
+}
